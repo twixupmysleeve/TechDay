2c8f8e41ec1eef5ae8490b57981bab29454e45dd
twixupmysleeve <pratham.mehta001@gmail.com>
2021-10-22 22:31:12 -0400

    Converted to list of tuples

--- Event.py
+++ Event.py
@@ -3,13 +3,13 @@ import pandas as pd
 import numpy as np
 
 class Event:
-    def __init__(self, name, location, duration, timeframe: Tuple, fixed=False):
+    def __init__(self, name: String, location: String, duration: int, timeframe: List, fixed=False):
         """
         Event class
         :param name: name of Event
         :param location: location of Event
         :param duration: time in minutes
-        :param timeframe: tuple => (start time in hrs, end time in hrs)
+        :param timeframe: list of tuples => [(start time in hrs, end time in hrs)]
         :param fixed: boolean representing variability
         """
         self.name = name
@@ -18,5 +18,5 @@ class Event:
         self.duration = duration
         self.fixed = fixed
 
-math1552 = Event("Math 1552", "Skiles", 50, (1100, 1150), fixed=True)
+math1552 = Event("Math 1552", "Skiles", 50, [(1100, 1150)], fixed=True)
 
